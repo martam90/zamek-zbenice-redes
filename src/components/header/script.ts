192112e19b106b968b5ad703c6783178298996d6
martam90 <banas.m.marta@gmail.com>
2026-07-04 15:09:21 +0200

    redes fixes upon release

--- src/components/header/script.ts
+++ src/components/header/script.ts
@@ -1,72 +1,82 @@
 interface navProps {
-  html: HTMLElement;
-  body: HTMLElement;
-  navDropdown: HTMLElement | null;
-  summary: HTMLElement | null;
-  detailsNav: HTMLDetailsElement | null;
-  init(): void;
-  handleEsc(): void;
-  handleOverlay(): void;
-  handleAria(): void;
-  handleFocus(): void;
+	html: HTMLElement;
+	body: HTMLElement;
+	navDropdown: HTMLElement | null;
+	summary: HTMLElement | null;
+	detailsNav: HTMLDetailsElement | null;
+	init(): void;
+	handleEsc(): void;
+	handleOverlay(): void;
+	handleAria(): void;
+	handleFocus(): void;
+	closeMobileNav(): void;
 }
 
 const NAV: navProps = {
-  html: document.documentElement,
-  body: document.body,
-  navDropdown: document.getElementById("nav"),
-  detailsNav: document.getElementById("details-nav") as HTMLDetailsElement,
-  summary: document.getElementById("summary-nav") as HTMLElement,
+	html: document.documentElement,
+	body: document.body,
+	navDropdown: document.getElementById('nav'),
+	detailsNav: document.getElementById('details-nav') as HTMLDetailsElement,
+	summary: document.getElementById('summary-nav') as HTMLElement,
 
-  handleOverlay() {
-    if (this.body) {
-      this.detailsNav?.addEventListener("toggle", () => {
-        this.body.classList.toggle("nav-overlay");
-        this.html.classList.toggle("js-is-open");
-      });
-    }
-  },
+	handleOverlay() {
+		if (this.body) {
+			this.detailsNav?.addEventListener('toggle', () => {
+				this.body.classList.toggle('nav-overlay');
+				this.html.classList.toggle('js-is-open');
+			});
+		}
+	},
 
-  handleAria() {
-    if (this.navDropdown) {
-      this.detailsNav?.addEventListener("toggle", () => {
-        if (this.detailsNav?.hasAttribute("open")) {
-          this.navDropdown?.setAttribute("aria-expanded", "true");
-        } else {
-          this.navDropdown?.setAttribute("aria-expanded", "false");
-        }
-      });
-    }
-  },
+	handleAria() {
+		if (this.navDropdown) {
+			this.detailsNav?.addEventListener('toggle', () => {
+				if (this.detailsNav?.hasAttribute('open')) {
+					this.navDropdown?.setAttribute('aria-expanded', 'true');
+				} else {
+					this.navDropdown?.setAttribute('aria-expanded', 'false');
+				}
+			});
+		}
+	},
 
-  handleEsc() {
-    document.addEventListener("keydown", (e) => {
-      if (e.key === "Escape") {
-        this.detailsNav?.removeAttribute("open");
-      }
-    });
-  },
+	handleEsc() {
+		document.addEventListener('keydown', (e) => {
+			if (e.key === 'Escape') {
+				this.detailsNav?.removeAttribute('open');
+			}
+		});
+	},
 
-  handleFocus() {
-    this.detailsNav?.addEventListener("toggle", () => {
-      const focusableElems = this.navDropdown?.querySelectorAll(
-        'a, button, input, select, textarea, [tabindex]:not([tabindex="-1"])',
-      ) as NodeListOf<HTMLAnchorElement>;
+	handleFocus() {
+		this.detailsNav?.addEventListener('toggle', () => {
+			const focusableElems = this.navDropdown?.querySelectorAll(
+				'a, button, input, select, textarea, [tabindex]:not([tabindex="-1"])'
+			) as NodeListOf<HTMLAnchorElement>;
 
-      if (this.detailsNav?.open) {
-        focusableElems[0]?.focus();
-      } else {
-        this.summary?.focus();
-      }
-    });
-  },
+			if (this.detailsNav?.open) {
+				focusableElems[0]?.focus();
+			} else {
+				this.summary?.focus();
+			}
+		});
+	},
 
-  init() {
-    this.handleOverlay();
-    this.handleAria();
-    this.handleEsc();
-    this.handleFocus();
-  },
+	closeMobileNav() {
+		this.detailsNav?.querySelectorAll('a').forEach((link) => {
+			link?.addEventListener('click', () => {
+				this.detailsNav?.removeAttribute('open');
+			});
+		});
+	},
+
+	init() {
+		this.handleOverlay();
+		this.handleAria();
+		this.handleEsc();
+		this.handleFocus();
+		this.closeMobileNav();
+	},
 };
 
 NAV.init();
